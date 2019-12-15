0acea76806391c95639b706c0d00ee88b3700119
KimuraRyo <ryki@mac.com>
2019-12-15 14:35:14 +0900

    Enabled high DPI settings of Qt

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -35,7 +35,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget* parent);
+    explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
 public slots:
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 // =================================================================== //
-// Copyright (C) 2014-2015 Kimura Ryo                                  //
+// Copyright (C) 2014-2019 Kimura Ryo                                  //
 //                                                                     //
 // This Source Code Form is subject to the terms of the Mozilla Public //
 // License, v. 2.0. If a copy of the MPL was not distributed with this //
@@ -14,14 +14,15 @@
 int main(int argc, char** argv)
 {
     QLocale::setDefault(QLocale::system());
+
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
     QApplication app(argc, argv);
-    MainWindow* mainWindow = new MainWindow(0);
+    MainWindow* mainWindow = new MainWindow;
     mainWindow->show();
 
     const QStringList arguments = app.arguments();
-
     if (arguments.size() > 1 && QFile::exists(arguments.last())) {
         mainWindow->openFile(arguments.last());
     }
