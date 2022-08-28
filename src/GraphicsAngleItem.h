4b38266ec8b626b5fe458d5f8cd5e58ddbed9c76
KimuraRyo <ryki@mac.com>
2022-08-28 19:47:30 +0900

    Fixed a parameter name

--- src/GraphicsAngleItem.h
+++ src/GraphicsAngleItem.h
@@ -27,7 +27,7 @@ public:
 
     QRectF boundingRect() const;
     QPainterPath shape() const;
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem* item, QWidget* widget);
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem* option, QWidget* widget);
 
 private:
     QColor color_;
